55bef795efa348a7ddc4c9ac3df72dfccad4ad04
Bernardo Deuchmann <bernardodeuchmann@gmail.com>
2026-04-12 18:04:22 -0300

    feat: adiciona um relatório financeiro completo e main.py
    - Atualiza generate_report com totais separados
    - Adiciona total de receitas e despesas
    - Cria main.py como um ponto de entrada no sistema

--- src/report.py
+++ src/report.py
@@ -1,9 +1,20 @@
-from transaction import Transaction
+from src.transaction import Transaction
 
 def generate_report(transaction: Transaction):
     print("==== RELATÓRIO FINANCEIRO ====")
+
+    total_receitas = 0
+    total_despesas = 0
+
     for t in transaction.list_all():
-        symbol = "+" if t["type"] == "receita" else "-"
-        print(f"{symbol} {t['description']}: R${t['amount']:.2f}")
+        if t["type"] == "receita":
+            total_receitas += t["amount"]
+            print(f"+ {t['description']}: R${t['amount']:.2f}")
+        else:
+            total_despesas += t["amount"]
+            print(f"- {t['description']}: R${t['amount']:.2f}")
+
     print ("--------------------------")
+    print(f"Total de receitas: R${total_receitas:.2f}")
+    print(f"Total de despesas: R${total_despesas:.2f}")
     print(f"Saldo final: R${transaction.get_balance():.2f}")
